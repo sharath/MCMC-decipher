7000f6f224bad0faec95de16e01253bba2560dc7
sharath <sharathramku@umass.edu>
2018-04-23 23:00:34 -0400

    Implemented Hill Climbing and Simulated Annealing

--- Runner.py
+++ Runner.py
@@ -1,4 +1,29 @@
 import random
+import numpy as np
+
+
+class HillClimbingRunner:
+    """
+    g: proposal density
+    s: scrambled text
+    x: initial candidate
+    xs: function for picking next candidate
+    f: evaluation function for candidate
+    """
+
+    def __init__(self, g, s, x, xs, f):
+        self.g = g
+        self.s = s
+        self.x = x
+        self.xs = xs
+        self.f = f
+
+    # runs iterations of the hill-climbing algorithm
+    def run(self):
+        xp = self.xs(self.x)
+        a = self.f(self.x, self.s, self.g) / self.f(xp, self.s, self.g)
+        if a > 1:
+            self.x = xp
 
 
 class MetropolisRunner:
@@ -28,12 +53,12 @@ class MetropolisRunner:
 
 class MetropolisHastingsRunner:
     """
-        g: proposal density
-        s: scrambled text
-        x: initial candidate
-        xs: function for picking next candidate
-        f: evaluation function for candidate
-        """
+    g: proposal density
+    s: scrambled text
+    x: initial candidate
+    xs: function for picking next candidate
+    f: evaluation function for candidate
+    """
 
     def __init__(self, g, s, x, xs, f):
         self.g = g
@@ -42,10 +67,41 @@ class MetropolisHastingsRunner:
         self.xs = xs
         self.f = f
 
-    # runs iterations of the metropolis algorithm
+    # runs iterations of the metropolis-hastings algorithm
     def run(self):
         xp = self.xs(self.x)
         if self.f(xp, self.s, self.g) > self.f(self.x, self.s, self.g):
             self.x = xp
         elif random.uniform(0, 1) <= self.f(xp, self.s, self.g) / self.f(self.x, self.s, self.g):
+            self.x = xp
+
+
+class SimulatedAnnealingRunner:
+    """
+    g: proposal density
+    s: scrambled text
+    x: initial candidate
+    xs: function for picking next candidate
+    f: evaluation function for candidate
+    t: starting temperature
+    rate: rate of annealing
+        """
+
+    def __init__(self, g, s, x, xs, f, t=100, rate=0.3):
+        self.g = g
+        self.s = s
+        self.x = x
+        self.xs = xs
+        self.f = f
+        self.t = t
+        self.rate = rate
+
+    # runs iterations of the simulated-annealing algorithm
+    def run(self):
+        self.t = max(0.00000001, self.t * self.rate)
+        xp = self.xs(self.x)
+        delta = self.f(xp, self.s, self.g) - self.f(self.x, self.s, self.g)
+        if delta > 0:
+            self.x = xp
+        elif np.exp(delta / self.t) > random.uniform(0, 1):
             self.x = xp
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from Runner import *
 from TextCrawler import TextCrawler
 from Scrambler import Scrambler
-import numpy as np
-from scipy.misc import logsumexp
 import random
 
 
@@ -13,7 +11,7 @@ def f(x, s, dist):
             p *= 0.00001
             continue
         p *= dist[x[s[i]]][x[s[i + 1]]]
-    return np.exp(logsumexp(-3 * p) - logsumexp(-3 * p))
+    return p
 
 
 def xs(x=None):
@@ -22,7 +20,7 @@ def xs(x=None):
         x = dict()
         for c in alphabet:
             x[c] = c
-        return x
+        return dict(zip(random.sample(x.keys(), len(x.keys())), x.values()))
     k1, k2 = random.sample(x.keys(), 2)
     xp = dict(x)
     xp[k1], xp[k2] = xp[k2], xp[k1]
@@ -31,15 +29,14 @@ def xs(x=None):
 
 def main():
     g = TextCrawler('ProjectGutenberg/Hamlet.txt').cond_prob()
-    s, answer = Scrambler().scramble('ENTER HAMLET HAMLET TO BE OR NOT TO BE THAT IS '
-                                     'THE QUESTION WHETHER TIS NOBLER IN THE MIND TO '
-                                     'SUFFER THE SLINGS AND ARROWS OF OUTRAGEOUS FORT'
-                                     'UNE OR TO TAKE ARMS AGAINST A SEA OF TROUBLES A'
-                                     'ND BY OPPOSING END')
-    runner = MetropolisRunner(g, s, xs(), xs, f)
+    s, answer = Scrambler().scramble('ENTER HAMLET HAMLET TO BE OR NOT TO BE THAT IS')
+    runners = [SimulatedAnnealingRunner, MetropolisHastingsRunner, MetropolisRunner, HillClimbingRunner]
+    r = 0
+    print(runners[r])
+    runner = runners[r](g, s, xs(), xs, f)
     i = 0
-    while runner.x is not answer:
-        if i % 20000 == 0:
+    while Scrambler().unscramble_with_cipher(s, runner.x) != 'ENTER HAMLET HAMLET TO BE OR NOT TO BE THAT IS':
+        if i % 2000 == 0:
             print(Scrambler().unscramble_with_cipher(s, runner.x))
         runner.run()
         i += 1
