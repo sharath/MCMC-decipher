493d0c06ad932b9053f19a4c79da5fda1b9f79b8
sharath <sharathramku@umass.edu>
2018-05-01 22:44:20 +0200

    final commit before presentation

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
 import re
 import string
+import numpy as np
 
 alphabet = string.ascii_uppercase + ' '
 
@@ -21,6 +22,18 @@ def cbpd_from_book(book):
             g[c1][c2] = g[c1][c2] / total
 
     return g
+
+def cbfd_from_book(book):
+    text = sanitize(open(book, 'r').read())
+    h = np.zeros((len(alphabet), len(alphabet)))
+    for c1 in alphabet:
+        for c2 in alphabet:
+            h[alphabet.index(c1)][alphabet.index(c2)] = 0
+
+    for i in range(0, len(text) - 1):
+        h[alphabet.index(text[i])][alphabet.index(text[i + 1])] += 1
+        
+    return h
 
 
 def sanitize(text):
